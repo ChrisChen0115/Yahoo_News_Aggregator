f19dbebd7a6460768a438921533c988a16d69b8a
Chris <stonecold608123@gmail.com>
2025-10-29 16:11:33 +0800

    Add News model and register to admin

---
+++ Yahoo_Project/Yahoo_Project/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Yahoo_Project project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Yahoo_Project.settings')
+
+application = get_asgi_application()
